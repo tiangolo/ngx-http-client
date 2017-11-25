a16156dfce356b2def97594ca9bdbc268f124922
Sebastián Ramírez <tiangolo@gmail.com>
2017-11-25 12:26:58 +0300

    Start repo with minimum changes based on official Angular

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+const globals = {
+  '@angular/core': 'ng.core',
+  '@angular/platform-browser': 'ng.platformBrowser',
+  '@angular/common': 'ng.common',
+  'rxjs/Observable': 'Rx',
+  'rxjs/Observer': 'Rx',
+  'rxjs/Subject': 'Rx',
+
+  'rxjs/observable/of': 'Rx.Observable.prototype',
+
+  'rxjs/operator/concatMap': 'Rx.Observable.prototype',
+  'rxjs/operator/filter': 'Rx.Observable.prototype',
+  'rxjs/operator/map': 'Rx.Observable.prototype',
+};
+
+export default {
+  input: 'dist/index.js',
+  output: {
+    file: 'dist/bundles/http.umd.js',
+    format: 'umd',
+  },
+  name: 'ngxHttpClient',
+  external: Object.keys(globals),
+  globals: globals
+};
